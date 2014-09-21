33ec44f059d46286a9f0824abbbc41261d4a16df
Andrey Teplyashin <platinumthinker@gmail.com>
2014-09-21 13:29:06 +0700

    Add scripts for list windows

--- scripts/winlist.sh
+++ scripts/winlist.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 DMENU="dmenu.xft -fn \"FiraMono-11\" -nb #002b36 -nf #657b83 -sb #002b36 -sf #fdce59"
 
-wnds=$(wmctrl -l | awk '{print substr($0, index($0,$4))}')
+deskID=$(wmctrl -d | grep "  \* " | grep -o -E "[0-9]+[[:space:]]")
+mask="0x[0-9a-z]+[[:space:]]+$deskID"
+wnds=$(wmctrl -l | grep -E "$mask" | awk '{print substr($0, index($0,$4))}' )
 
 sel=$(echo "$wnds" | sort -u | $DMENU)
+
 test -z "$sel" && exit 0
 wrkid=`wmctrl -d | grep "${sel}" | awk '{print $1}'`
 test $wrkid && wmctrl -s $wrkid || wmctrl -a "$sel" || exit 1
