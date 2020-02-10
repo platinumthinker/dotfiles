6d72454c440f97bba586ba1017f657528d43015b
Andrey Teplyashin <andrey.teplyashin@bitfury.com>
2020-02-10 10:58:53 +0700

    Update config

--- scripts/pactrl.sh
+++ scripts/pactrl.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-VOLUME_FILE=/home/thinker/scripts/pa_volume
 MUTE_FILE=/home/thinker/scripts/pa_mute
 ICON_PATH=/usr/share/icons/Faenza/status/48
 
+function getVolume() {
+    amixer get Master | grep -Eo '[0-9]+%' | head -1
+}
+
 function doMute() {
-	NEW_VOLUME=$(amixer get Master | egrep -o '[0-9]+%')
+	NEW_VOLUME=$(getVolume)
 	NEW_VOLUME=${NEW_VOLUME%%%}
-	MUTE_FILE=`amixer get Master | egrep 'Playback.*?\[o' | egrep -o '\[o.+\]'`
+    MUTE_FILE=$(amixer get Master | grep -E 'Playback.*?\[o' | grep -Eo '\[o.+\]')
     if [[ $MUTE_FILE == '[on]' ]];
     then
         amixer set Master mute
@@ -19,30 +22,30 @@ function doMute() {
 }
 
 function doChangeVolume() {
-	amixer -q sset Master $1	
-	NEW_VOLUME=$(amixer get Master | egrep -o '[0-9]+%')
+	amixer -q sset Master "$1"
+	NEW_VOLUME=$(getVolume)
 	NEW_VOLUME=${NEW_VOLUME%%%}
-	if [ $NEW_VOLUME -lt 0 ];
+	if [ "$NEW_VOLUME" -lt 0 ];
     then
         exit 0
     fi
-	if [ $NEW_VOLUME -eq 0 ];
+	if [ "$NEW_VOLUME" -eq 0 ];
 	then
 		ICON=$ICON_PATH/audio-volume-muted.png
-	elif [ $NEW_VOLUME -lt 25 ];
+	elif [ "$NEW_VOLUME" -lt 25 ];
 	then
 		ICON=$ICON_PATH/audio-volume-off.png
-	elif [ $NEW_VOLUME -lt 50 ];
+	elif [ "$NEW_VOLUME" -lt 50 ];
 	then
 		ICON=$ICON_PATH/audio-volume-low.png
-	elif [$NEW_VOLUME < 75]
+	elif [ "$NEW_VOLUME" -lt 75 ];
 	then
 		ICON=$ICON_PATH/audio-volume-medium.png
 	else
 		ICON=$ICON_PATH/audio-volume-high.png
 	fi
-	killall xfce4-notifyd
-	notify-send "M" -h int:value:$NEW_VOLUME -i $ICON&
+	# killall xfce4-notifyd
+	# notify-send "M" -h int:value:"$NEW_VOLUME" -i $ICON&
 }
 
 case $1 in
