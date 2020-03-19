59594df5ea29d93eaae3f1b4619d3de3269237d8
Andrey Teplyashin <andrey.teplyashin@bitfury.com>
2020-03-19 10:37:10 +0700

    Delete duplicate code

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 def=$(grep -E "design capacity:" /proc/acpi/battery/BAT0/info | grep -o -e "[0-9]\+")
 last=$(grep -E 'last full capacity:' /proc/acpi/battery/BAT0/info | grep -o -e "[0-9]\+")
