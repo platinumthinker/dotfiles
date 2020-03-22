b285d0dfa1887c23bf3361e55c778ff525f268a1
Andrey Teplyashin <andrey.teplyashin@bitfury.com>
2020-03-22 22:52:50 +0700

    Add script for detect and unmount unreacheble NFS mount point

---
+++ scripts/fix_freeze_root_with_nfs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+for MNT_PATH in $(findmnt --types nfs4 --output "target" --noheadings --raw); do
+    if [ -z "$MNT_PATH" ]; then
+        continue;
+    fi
+
+    ADDR=$(findmnt --target "$MNT_PATH" --noheadings --output "options" --raw \
+        | grep -Eo ',addr=[0-9]+(.[0-9]+)*' \
+        | grep -Eo '[0-9]+(.[0-9]+)*') || true
+
+    if [ -z "$ADDR" ]; then
+        # If mount point isn't NFS share
+        continue;
+    fi
+    # Check if the server is reachable
+    timeout 1 bash -c ": < /dev/tcp/${ADDR}1/2049" || {
+        echo "UnReachible $MNT_PATH"
+        umount -f -l "$MNT_PATH"
+    }
+done
