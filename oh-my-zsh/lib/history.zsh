fdcce371b0c739c5b7fecd2acc1992aad7f19a33
Platinum Thinker <platinumthinker@gmail.com>
2022-05-24 17:10:59 +0700

    Move history zsh config from zshrc to custom folder for stable behaviour

---
+++ oh-my-zsh/lib/history.zsh
@@ -0,0 +1,34 @@
+## History wrapper
+function omz_history {
+  local clear list
+  zparseopts -E c=clear l=list
+
+  if [[ -n "$clear" ]]; then
+    # if -c provided, clobber the history file
+    echo -n >| "$HISTFILE"
+    fc -p "$HISTFILE"
+    echo >&2 History file deleted.
+  elif [[ -n "$list" ]]; then
+    # if -l provided, run as if calling `fc' directly
+    builtin fc "$@"
+  else
+    # unless a number is provided, show all history events (starting from 1)
+    [[ ${@[-1]-} = *[0-9]* ]] && builtin fc -l "$@" || builtin fc -l "$@" 1
+  fi
+}
+
+# Timestamp format
+alias history="omz_history -t 'dd/mm/yyyy'"
+
+## History file configuration
+HISTFILE="$HOME/.zsh_history"
+HISTSIZE=5000000
+SAVEHIST=5000000
+
+## History command configuration
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt hist_ignore_all_dups   # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt hist_save_no_dups
+setopt share_history          # share command history data
